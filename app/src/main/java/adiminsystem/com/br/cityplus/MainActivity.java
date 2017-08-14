71081db706e058c1d393472ff119b1892767a5ef
Murilo Henrique Bezerra Silva <henrybezerra10@outlook.com>
2017-08-14 08:56:04 -0300

    Commit Inicial

---
+++ app/src/main/java/adiminsystem/com/br/cityplus/MainActivity.java
@@ -0,0 +1,13 @@
+package adiminsystem.com.br.cityplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
